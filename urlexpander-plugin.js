14f5a2172f30cbe5a753abb98f88a39354911a34
yasiral <yasir.altaf.ab@gmail.com>
2024-07-28 03:02:18 +0500

    Update urlexpander-plugin.js

--- urlexpander-plugin.js
+++ urlexpander-plugin.js
@@ -9,26 +9,16 @@ jQuery(document).ready(function($) {
             url: urlexpander_vars.ajax_url,
             data: {
                 action: 'urlexpander',
-				nonce: urlexpander_ajax.nonce,
+		nonce: urlexpander_ajax.nonce,
                 short_url: shortUrl
             },
             beforeSend: function() {
-                    $('#rlexpander-result').html('Expanding URL...');
+                    $('#urlexpander-result').html('Expanding URL...');
                 },
             success: function(response) {
-                    if (response.success) {
-                        $('#rlexpander-result').html('Expanded URL: <a href="' + response.data.expanded_url + '" target="_blank">' + response.data.expanded_url + '</a>');
-                    } else {
-                        $('#rlexpander-result').html('Error: ' + response.data);
-                    }
-                },
-                error: function() {
-                    $('#rlexpander-result').html('An error occurred. Please try again.');
-                }
-            });
-        } else {
-            $('#rlexpander-result').html('Please enter a URL to expand.');
-        }
+                $('#urlexpander-result').html(response);
+            }
+        });
     });
 });
 
