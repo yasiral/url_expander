2415ab5b8044a1dfda57fb51f4526134f5da6901
yasiral <yasir.altaf.ab@gmail.com>
2024-07-28 17:54:23 +0500

    Update urlexpander-plugin.js

--- urlexpander-plugin.js
+++ urlexpander-plugin.js
@@ -17,7 +17,11 @@ jQuery(document).ready(function($) {
                 },
             success: function(response) {
                 if (response.success && response.data) {
-                    $('#urlexpander-result').html(response.data.expanded_url); // Adjust depending on actual API response structure
+                  //  $('#urlexpander-result').html(response.data.expanded_url); // Adjust depending on actual API response structure
+		    var expandedUrl = response.data.expanded_url;
+                    var formattedResponse = '<p style="font-weight: bold; font-size: 14px; text-decoration: underline;">Here is your full URL:</p>';
+                    formattedResponse += '<a href="' + expandedUrl + '" target="_blank">' + expandedUrl + '</a>';
+                    $('#urlexpander-result').html(formattedResponse);
                 } else {
                     $('#urlexpander-result').html('Failed to expand URL');
                 }
